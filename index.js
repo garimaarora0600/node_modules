2161c3001c6b88e14950cc205557a52b9fd8cdbc
Garima Arora <112503036+garimaarora0600@users.noreply.github.com>
2024-02-02 16:52:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const calculator=require('./locAL_module');
+
+let x=50,y=50;
+console.log("Addition: "+ calculator.add(x,y));
+console.log("Subtraction: "+ calculator.sub(x,y));
+console.log("Multiplication: "+calculator.mul(x,y));
+console.log("Division:"+calculator.div(x,y));
